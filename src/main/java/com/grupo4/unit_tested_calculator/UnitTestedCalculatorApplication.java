6c68baba27bce44584d92dd6fb3dd82818a81067
EdoAbarca <eduardo.abarca.c@usach.cl>
2025-06-23 13:22:42 -0400

    chore: Se inicializa app Java Maven Spring Boot

---
+++ src/main/java/com/grupo4/unit_tested_calculator/UnitTestedCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.grupo4.unit_tested_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestedCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestedCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo4/unit_tested_calculator/UnitTestedCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo4.unit_tested_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestedCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
